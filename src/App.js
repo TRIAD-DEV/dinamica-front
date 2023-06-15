2afd7f37d01151ee9e864a13f1dae24a19ec1ee1
“triad-pedro-bastos” <pedro.bastos@triadpesquisa.com.brgit config --global user.email pedro.bastos@triadpesquisa.com.br>
2023-06-15 11:30:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
 import './App.css';
+import { List, ListItem, Typography, ListItemText, Avatar, ListItemAvatar, Divider, ListSubheader, Box } from '@mui/material';
+import { useEffect, useState ,Fragment, React, useLayoutEffect } from 'react';
 
 function App() {
+
+  async function getAnotacoes() {
+    try {
+      const response = await axios.get('http://localhost:8080/dinamica/anotacoes/listar');
+      setAnotacoes(response.data)
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const [anotacoes, setAnotacoes] = useState([])
+
+  useEffect(() => {
+    getAnotacoes()
+  }, [])
+
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#1C1C1C"
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box width='100%' height='45vw' display='flex' flexDirection='row' justifyContent='center' alignItems='center'>
+        <List sx={{
+          width: '100%',
+          maxWidth: 360,
+          bgcolor: 'background.paper',
+          position: 'relative',
+          overflow: 'auto',
+          maxHeight: 300,
+          '& ul': { padding: 0 }}}
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Anotações
+            </ListSubheader>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {
+          anotacoes.map((anotacao, index) => 
+            <>
+              <ListItem>
+                <ListItemText primary={anotacao.titulo} secondary={`Código: ${anotacao.codigo}`}/>
+              </ListItem>
+              {anotacoes.length -1 !== index && <Divider/>}
+            </>
+            )            
+          }
+        </List>
+    </Box>
   );
 }
 
